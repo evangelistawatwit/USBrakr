b04b7e2dc9ed84f3e397bf9f59b087d423c029a5
evangelistawatwit <90535502+evangelistawatwit@users.noreply.github.com>
2025-06-03 10:41:57 -0400

    Added csv reading and searching, also a placeholder csv list

--- main.py
+++ main.py
@@ -6,6 +6,11 @@
 import hashlib
 import sys
 import os
+import csv
+import pandas as pd # used for getting the specific columns
+
+# just having this here to test the CSV compatibility
+csv_list = "malwarebazaar_list.csv"
 
 # I'll condense the open in binary parts into it's own function - done
 # Condensed it all into one function
@@ -22,7 +27,32 @@ def get_hash(file_path, hash_func):
             hash_object.update(byte_block)
     f.close()
     # return the hex representation of the hash
-    return hash_object.hexdigest()
+    return('"' + hash_object.hexdigest() + '"')
+
+# Returns the gievn file's hash if it exists in the given CSV file
+def compare_to_csv(file_path):
+    # read the CSV file
+    mb_list = pd.read_csv(csv_list, on_bad_lines='skip', skiprows=[0,1,2,3,4,5,6,7], usecols=[1,2,3,4], header=None)
+    # get the hash of the file
+    md5 = get_hash(file_path, hashlib.md5)
+    sha1 = get_hash(file_path, hashlib.sha1)
+    sha256 = get_hash(file_path, hashlib.sha256)
+    
+    # check if the hashes are in the CSV file
+    if md5 in mb_list[2].values:
+        print(f"MD5 hash {md5} found in CSV file.")
+    else:
+        print(f"MD5 hash {md5} not found in CSV file.")
+    
+    if sha1 in mb_list[3].values:
+        print(f"SHA1 hash {sha1} found in CSV file.")
+    else:
+        print(f"SHA1 hash {sha1} not found in CSV file.")
+    
+    if sha256 in mb_list[1].values:
+        print(f"SHA256 hash {sha256} found in CSV file.")
+    else:
+        print(f"SHA256 hash {sha256} not found in CSV file.")
 
 # main function
 def main():
@@ -48,7 +78,10 @@ def main():
     print(f"Size: {size} bytes")
     print(f"MD5: {md5}")
     print(f"SHA1: {sha1}")
-    print(f"SHA256: {sha256}")
+    print(f"SHA256: {sha256} \n")
+    
+    # compare the hashes to the CSV file
+    compare_to_csv(path)
     # return 0
     return 0
 
